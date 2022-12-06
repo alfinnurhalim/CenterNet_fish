7989b92ca36c7a945adfc7010d77d32de762e181
Alfin Nurhalim <alfin.nurhalim@gmail.com>
2022-12-06 07:43:14 +0700

    fix resolution

--- src/lib/datasets/dataset/kitti.py
+++ src/lib/datasets/dataset/kitti.py
@@ -16,7 +16,7 @@ import torch.utils.data as data
 
 class KITTI(data.Dataset):
   num_classes = 3
-  default_resolution = [1024, 1024]
+  default_resolution = [512, 512]
   mean = np.array([0.485, 0.456, 0.406], np.float32).reshape(1, 1, 3)
   std = np.array([0.229, 0.224, 0.225], np.float32).reshape(1, 1, 3)
 
--- src/lib/utils/post_process.py
+++ src/lib/utils/post_process.py
@@ -10,14 +10,15 @@ from .ddd_utils import ddd2locrot
 def get_pred_depth(depth):
   return depth
 
-def get_alpha(rot):
+def get_alpha(rot,is_alphay = False):
   # output: (B, 8) [bin1_cls[0], bin1_cls[1], bin1_sin, bin1_cos, 
   #                 bin2_cls[0], bin2_cls[1], bin2_sin, bin2_cos]
   # return rot[:, 0]
-  idx = rot[:, 1] > rot[:, 5]
-  alpha1 = np.arctan2(rot[:, 2], rot[:, 3]) + (-0.5 * np.pi)
-  alpha2 = np.arctan2(rot[:, 6], rot[:, 7]) + ( 0.5 * np.pi)
-  return alpha1 * idx + alpha2 * (1 - idx)
+  mul = int(is_alphay)*8
+  idx = rot[:, mul+1] > rot[:, mul+5]
+  alpha1 = np.arctan2(rot[:, mul+2], rot[:, mul+3])
+  alpha2 = np.arctan2(rot[:, mul+6], rot[:, mul+7])
+  return ((alpha1 * idx + alpha2 * (1 - idx)) + (2*np.pi))%(2*np.pi)
   
 
 def ddd_post_process_2d(dets, c, s, opt):
