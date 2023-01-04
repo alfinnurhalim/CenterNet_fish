cbd3e71db75329d16682ee4c128f528ebff11fef
Alfin Nurhalim <alfin.nurhalim@gmail.com>
2023-01-04 20:33:17 +0700

    fix frozen layer

--- src/main.py
+++ src/main.py
@@ -44,7 +44,7 @@ def main(opt):
     'wh' : model.wh,
     'reg' : model.reg,
   }
-  frozen_layer = ['base','dla_up','ida_up','hm','reg']
+  frozen_layer = ['base','dla_up','ida_up','hm','reg','wh']
   for key in frozen_layer:
     for name,param in model_layers[key].named_parameters():
         param.requires_grad = False
